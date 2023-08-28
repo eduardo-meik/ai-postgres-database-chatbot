1bf328ad1047e4cb2d625705a75a9fd9ed6775be
eduardo-meik <66313196+eduardo-meik@users.noreply.github.com>
2023-08-28 10:12:31 -0400

    Update config.py
    os.environ used instead of st.secrets

--- utils/config.py
+++ utils/config.py
@@ -1,33 +1,26 @@
 import streamlit as st
 import os
 
-
-# Set up Postgres database credentials using st.secrets
+# Set up Postgres database credentials using os.environ
 db_credentials = {
-    'dbname'    :   st.secrets["db_credentials"]["PGDATABASE"],
-    'user'      :   st.secrets["db_credentials"]["PGUSER"],
-    'password'  :   st.secrets["db_credentials"]["PGPASSWORD"],
-    'host'      :   st.secrets["db_credentials"]["PGHOST"],
-    'port'      :   st.secrets["db_credentials"]["PGPORT"]
+    'dbname': os.environ.get("PGDATABASE"),
+    'user': os.environ.get("PGUSER"),
+    'password': os.environ.get("PGPASSWORD"),
+    'host': os.environ.get("PGHOST"),
+    'port': os.environ.get("PGPORT")
 }
 
-
-
-# Set up OpenAI variables 
-OPENAI_API_KEY  =   st.secrets["OPENAI"]["OPENAI_API_KEY"]
-AI_MODEL        =   'gpt-3.5-turbo-16k'
-# AI_MODEL        =   'gpt-4'
-
-
+# Set up OpenAI variables
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+AI_MODEL = 'gpt-3.5-turbo-16k'
+# AI_MODEL = 'gpt-4'
 
 # Max number of tokens permitted within a conversation exchange via OpenAI API
-MAX_TOKENS_ALLOWED      =   3000
-
+MAX_TOKENS_ALLOWED = 3000
 
 # Max number of messages to exchange with OpenAI API
-MAX_MESSAGES_TO_OPENAI  =   5
-
-
+MAX_MESSAGES_TO_OPENAI = 5
 
 # An arbitrary number to provide a buffer to avoid reaching exact token limits
-TOKEN_BUFFER            =   100  
+TOKEN_BUFFER = 100
+
